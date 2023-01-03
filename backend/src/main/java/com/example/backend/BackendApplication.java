6eb49ea8a9dd5b381b3a99183ba4be33834a81b6
dilshan27 <damithaadilshan@gmail.com>
2023-01-03 09:49:04 +0530

    update

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,29 @@
 package com.example.backend;
 
+import com.example.backend.model.Customer;
+import com.example.backend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Override
+    public void run(String... args) throws Exception {
+      /*  Customer customer=new Customer();
+        customer.setId("Coo1");
+        customer.setFirstName("kamal");
+        customer.setLastName("gunathilaka");
+        customer.setEmail("gunathilaka@gmail.com");
+
+        customerRepository.save(customer);*/
+    }
 }
--- backend/src/main/java/com/example/backend/controller/CustomerController.java
+++ backend/src/main/java/com/example/backend/controller/CustomerController.java
@@ -3,17 +3,19 @@ package com.example.backend.controller;
 import com.example.backend.model.Customer;
 import com.example.backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/customer")
+@RequestMapping("/api/customers")
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @GetMapping
     public List<Customer> getAllCustomer() {
         return customerRepository.findAll();
     }
